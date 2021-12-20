02e45a566ecdb8ab2ec7d737e054383fc26bbf75
vniacsta <vaniarcosta26@gmail.com>
2021-12-20 11:09:26 +0000

    Changed website links

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="box">
-          <Search defaultCity="Porto" />
+    <div className='App'>
+      <div className='container'>
+        <div className='box'>
+          <Search defaultCity='Porto' />
         </div>
         <small>
           <a
-            href="https://github.com/vniacsta/react-app-project"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://github.com/vniacsta/react-app-project'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Open-source code
           </a>
-          , by{" "}
+          , by{' '}
           <a
-            href="https://vniacsta.com/"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vniacsta.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vania Costa
           </a>
